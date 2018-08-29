39ec300800eea593c3524689a9bfc395c5c516fd
Tor Kjetil Nilsen <tor@arkitektum.no>
2018-08-29 12:07:49 +0200

    wip

---
+++ dibk.digitek.veiviser/Scripts/brann.js
@@ -0,0 +1,41 @@
+﻿var vueBrannModel = new Vue({
+    el: '#vue-brann',
+    data: {
+        listElements: [],
+        schemaTypes: schemaTypes,
+        selectedSchemaType: {},
+        milestones: milestones,
+        enterpriseTerms: enterpriseTerms,
+        selectedMilestone: '',
+        selectedEnterpriseterms: [],
+        listOfSelectedKeys: []
+    },
+    components: {
+        listElement: ListElement
+    },
+    methods: {
+        getActivities: function () {
+            showLoadingAnimation();
+            var queryString = '/api/sjekkliste/' + this.selectedSchemaType.Value;
+
+            axios.get(queryString)
+                .then(function (response) {
+                    console.log(response);
+                    if (response.data !== undefined) {
+                        this.listElements = response.data;
+                    }
+                    hideLoadingAnimation();
+                }.bind(this))
+                .catch(function (error) {
+                    console.log(error);
+                    hideLoadingAnimation();
+                });
+        },
+
+        showDescription: function () {
+            console.log(listElement.Beskrivelse);
+        },
+
+
+    }
+});
