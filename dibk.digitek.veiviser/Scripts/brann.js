5766f91946c7b4c85f898d1151224a3402233b68
Benjamin Dehli <benjamin@arkitektum.no>
2018-08-30 13:25:08 +0200

    Add vue logic and template for wizard 🧙

--- dibk.digitek.veiviser/Scripts/brann.js
+++ dibk.digitek.veiviser/Scripts/brann.js
@@ -1,41 +1,35 @@
-﻿var vueBrannModel = new Vue({
+﻿
+
+var vueBrannModel = new Vue({
     el: '#vue-brann',
     data: {
-        listElements: [],
-        schemaTypes: schemaTypes,
-        selectedSchemaType: {},
-        milestones: milestones,
-        enterpriseTerms: enterpriseTerms,
-        selectedMilestone: '',
-        selectedEnterpriseterms: [],
-        listOfSelectedKeys: []
+        schemaTypes: [
+            { key: '1', name: 'Risikoklasser', number: '1' },
+            { key: '2', name: 'Brannklasser', number: '2' },
+            { key: '3', name: 'Brannspredning mellom byggverk', number: '3' }
+        ],
+        selectedSchemaType: { key: '1', name: 'Risikoklasser', number: '1' },
+        variables: {
+            typeVirksomhet: { value: null, type: "String" },
+            antallEtasjer: { value: null, type: "long" },
+            brtArealPrEtasje: { value: null, type: "long" },
+            utgangTerrengAlleBoenheter: { value: null, type: "boolean" },
+            bareSporadiskPersonopphold: { value: null, type: "String" },
+            alleKjennerRomningsVeiene: { value: null, type: "boolean" },
+            bareBeregnetVakne: { value: null, type: "boolean" },
+            liteBrannfarligAktivitet: { value: null, type: "boolean" },
+            konsekvensAvBrann: { value: null, type: "String" },
+            brannBelastning: { value: null, type: "long" },
+            brannEnergi: { value: null, type: "long" },
+            bygningOffentligUnderTerreng: { value: null, type: "boolean" },
+            arealBrannseksjonPrEtasje: { value: null, type: "long" }
+        },
+        descriptions: null
     },
-    components: {
-        listElement: ListElement
+    created: function () {
+
     },
     methods: {
-        getActivities: function () {
-            showLoadingAnimation();
-            var queryString = '/api/sjekkliste/' + this.selectedSchemaType.Value;
-
-            axios.get(queryString)
-                .then(function (response) {
-                    console.log(response);
-                    if (response.data !== undefined) {
-                        this.listElements = response.data;
-                    }
-                    hideLoadingAnimation();
-                }.bind(this))
-                .catch(function (error) {
-                    console.log(error);
-                    hideLoadingAnimation();
-                });
-        },
-
-        showDescription: function () {
-            console.log(listElement.Beskrivelse);
-        },
-
-
+        
     }
 });
